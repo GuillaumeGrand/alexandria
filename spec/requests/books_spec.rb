2ecfcf3dacedfde545e86bd36bdca280c1a2502a
guillaume grand <guillaume.grand80@orange.fr>
2021-09-20 11:17:01 +0200

    Add BooksController, BookPresenter and FieldPicker

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -19,20 +19,41 @@ RSpec.describe 'Books', type: :request do
     # Before any test, let's create our 3 books
     before { books }
 
-    context 'default behavior' do
-        before { get '/api/books' }
+        context 'default behavior' do
+            before { get '/api/books' }
 
-        it 'gets HTTP status 200' do
-        expect(response.status).to eq 200
-        end
+            it 'gets HTTP status 200' do
+            expect(response.status).to eq 200
+            end
 
-        it 'receives a json with the "data" root key' do
-        expect(json_body['data']).to_not be nil
-        end
+            it 'receives a json with the "data" root key' do
+            expect(json_body['data']).to_not be nil
+            end
 
-        it 'receives all 3 books' do
-        expect(json_body['data'].size).to eq 3
+            it 'receives all 3 books' do
+            expect(json_body['data'].size).to eq 3
+            end
         end
-    end
+        describe 'field picking' do
+            context 'with the fields parameter' do
+              before { get '/api/books?fields=id,title,author_id' }
+      
+              it 'gets books with only the id, title and author_id keys' do
+                json_body['data'].each do |book|
+                  expect(book.keys).to eq ['id', 'title', 'author_id']
+                end
+              end
+            end
+      
+            context 'without the "fields" parameter' do
+              before { get '/api/books' }
+      
+              it 'gets books with all the fields specified in the presenter' do
+                json_body['data'].each do |book|
+                  expect(book.keys).to eq BookPresenter.build_attributes.map(&:to_s)
+                end
+              end
+            end
+          end
     end
 end
