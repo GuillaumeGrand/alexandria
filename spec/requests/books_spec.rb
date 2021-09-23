b1ce9c8637a9eba953f7825ab256cbe664484e8a
guillaume grand <guillaume.grand80@orange.fr>
2021-09-23 10:12:36 +0200

    Implement Representation Builders

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -55,53 +55,70 @@ RSpec.describe 'Books', type: :request do
               end
             end
           end
+
+          context 'with invalid field name "fid"' do
+            before { get '/api/books?fields=fid,title,author_id' }
+    
+            it 'gets "400 Bad Request" back' do
+              expect(response.status).to eq 400
+            end
+    
+            it 'receives an error' do
+              expect(json_body['error']).to_not be nil
+            end
+    
+            it 'receives "fields=fid" as an invalid param' do
+              expect(json_body['error']['invalid_params']).to eq 'fields=fid'
+            end
+          end 
         end
-        # describe 'pagination' do
-        #   context 'when asking for the first page' do
-        #     before { get('/api/books?page=1&per=2') }
+
+        describe 'pagination' do
+          context 'when asking for the first page' do
+            before { get('/api/books?page=1&per=2') }
     
-        #     it 'receives HTTP status 200' do
-        #       expect(response.status).to eq 200
-        #     end
+            it 'receives HTTP status 200' do
+              expect(response.status).to eq 200
+            end
     
-        #     it 'receives only two books' do
-        #       expect(json_body['data'].size).to eq 2
-        #     end
+            it 'receives only two books' do
+              expect(json_body['data'].size).to eq 2
+            end
     
-        #     it 'receives a response with the Link header' do
-        #       expect(response.headers['Link'].split(', ').first).to eq(
-        #         '<http://www.example.com/api/books?page=2&per=2>; rel="next"'
-        #       )
-        #     end
-        #   end
+            it 'receives a response with the Link header' do
+              expect(response.headers['Link'].split(', ').first).to eq(
+                '<http://www.example.com/api/books?page=2&per=2>; rel="next"'
+              )
+            end
+          end
     
-        #   context 'when asking for the second page' do
-        #     before { get('/api/books?page=2&per=2') }
+          context 'when asking for the second page' do
+            before { get('/api/books?page=2&per=2') }
     
-        #     it 'receives HTTP status 200' do
-        #       expect(response.status).to eq 200
-        #     end
+            it 'receives HTTP status 200' do
+              expect(response.status).to eq 200
+            end
     
-        #     it 'receives only one book' do
-        #       expect(json_body['data'].size).to eq 1
-        #     end
-        #   end
-        #   context "when sending invalid 'page' and 'per' parameters" do
-        #     before { get('/api/books?page=fake&per=10') }
+            it 'receives only one book' do
+              expect(json_body['data'].size).to eq 1
+            end
+          end
+          context "when sending invalid 'page' and 'per' parameters" do
+            before { get('/api/books?page=fake&per=sfs') }
     
-        #     it 'receives HTTP status 400' do
-        #       expect(response.status).to eq 400
-        #     end
+            it 'receives HTTP status 400' do
+              expect(response.status).to eq 400
+            end
     
-        #     it 'receives an error' do
-        #       expect(json_body['error']).to_not be nil
-        #     end
+            it 'receives an error' do
+              expect(json_body['error']).to_not be nil
+            end
     
-        #     it "receives 'page=fake' as an invalid param" do
-        #       expect(json_body['error']['invalid_params']).to eq 'page=fake'
-        #     end
-        #   end
-        # end
+            it "receives 'page=fake' as an invalid param" do
+              expect(json_body['error']['invalid_params']).to eq 'page=fake'
+            end
+          end
+        end
         describe 'sorting' do
           context 'with valid column name "id"' do
             it 'sorts the books by "id desc"' do
